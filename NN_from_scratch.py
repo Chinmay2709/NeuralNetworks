f85560ac278fe5617c96904cd2d1a55c3d5f0f2a
Chinmay <107140611+Chinmay2709@users.noreply.github.com>
2022-09-13 11:06:13 -0400

    NN Class defined but yet to be built

--- NN_from_scratch.py
+++ NN_from_scratch.py
@@ -4,17 +4,37 @@
 #    Chinmay Mishra
 #Date:
 #    9/10/2022
-import pandas as pd
+from numpy import *
 from sklearn.datasets import load_iris
 
 data = load_iris()
 
+print('Detailed Data listing:')
 print(data['data'].shape)
-
 print(data['target'].shape)
+print(data['target_names'])
 
-print(f"""
-{data['feature_names']}
-{data['target_names']}
-""")
+
+#Defining Neural Network Class:
+
+class Iris_Neural_Network():
+    def __init__(self):
+        pass
+    def Forward(self,X):
+        pass
+    def ActivationRelu(self):
+        pass
+    def ActivationRivRelu_2(self):
+        pass
+    def Loss_and_error(self):
+        pass
+    def Delta(self):
+        pass
+    def BackPropogation():
+        pass
+    def Learning():
+        pass
+
+
+#Initializing the variables X (Input) and Target (Output): 
 
